7faa6ad3ae5f9f73614fdbe174a8caa7cdb6f067
Vitaly Bokser <vbokser3@gatech.edu>
2018-07-15 19:14:46 -0400

    updated py file to accept more image formats handle case better by lowering the case of all files inputted

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Python script to change image names form the command line fast
 
 usage: change_file_names.py directory_where_images_are name_files_to
 
-ex. change_file_names.py /desktop/images new_image_name
+ex. change_file_names.py /desktop/images "my new images"
 
 
 More Info:
@@ -14,8 +14,14 @@ It renames all image files currently
 
 so if you just downloaded a bunch of pics from your vacation in Ohio, where all people go, you can take those cryptic jpg and png files and just rename them fast to something more meaningful.
 
-so a bunch of files like "IMG_0136.jpg" become "Ohio Trip 0001.jpg"
+so a bunch of files like "IMG_XXXX.jpg" become "Ohio Trip 0001.jpg"
 
 nothing fancy yet, simple utility.
 
 More to come.
+
+# Adding More Formats
+
+To add more formats just open the python file and append the format array to whatever format you want.
+
+
--- change_file_names.py
+++ change_file_names.py
@@ -1,22 +1,29 @@
 import os
 import sys
 
+#just update this global array to add whatever extensions you are woring with
+#yes global variables bad - blah blah blah
+image_file_extension_array = ["jpg", "png", "exif", "tiff", "gif"]
+
 
 def main():
-    """ use: change_file_names.py [directory_where_images_are] [name_files_to] [match_ext]
+    """ use: change_file_names.py directory_where_images_are name_files_to
     """
 
     if len(sys.argv) < 3:
-        print("error: usage: change_file_names.py [directory_where_images_are] [name_files_to]")
+        print("error: usage: change_file_names.py directory_where_images_are name_files_to")
+        print("ex. change_file_names.py /desktop/images new_image_name")
         return
 
     directory = sys.argv[1]
     name_files_to = sys.argv[2]
 
-    print("looking for directory: '{}'".format(directory))
+    print("\n************************ CHANGE IMAGE NAMES ************************\n")
+
+    print("\nAccessing directory: '{}'\n".format(directory))
+
     if check_if_valid_directory(directory):
         changeFileNames(directory, name_files_to)
-        print("Accessing Directory")
     else:
         print("Directory doesnt exist. Double Check the location and run again")
 
@@ -24,50 +31,59 @@ def main():
     #     print(arg)
 
 def check_if_valid_directory(directory):
-    return True
+    return (os.path.isdir(directory)) #is this a valid directory
 
 def changeFileNames(images_directory, name_files_to):
-    #cur_dir = os.getcwd()
+    #cur_dir = os.getcwd()'
 
-    # images_directory = "{}/{}".format(cur_dir, "images")
-    print(images_directory)
+    # image_file_extension_array = ["jpg", "png", "exif", "tiff", "gif"]
+
     #get files in curent directory
     file_list = os.listdir(images_directory)
 
     file_changed_counter = 0
 
+    #print("file list = {}".format(file_list))
+
     for i, filename in enumerate(file_list):
         #print("file {} : {}".format(i, filename))
         #os.rename(filename, filename[7:])
-        if filename.endswith(".jpg") or filename.endswith(".png"):
-            print("")
 
-            file_extension = "jpg"
+        if not os.path.isfile(os.path.join(images_directory, filename)):
+            print("")
+            print("Filename '{}' not a file -> skipping".format(filename))
+            continue
+
+        filename = filename.lower()
+        file_split = filename.split(".")
+        #print("img format = {}".format(file_split[-1]))
+        file_extension = file_split[-1] #take last element of array (should be img format)
+
+        if file_split[-1] in image_file_extension_array:
+
             image_number = str(file_changed_counter).zfill(4) #counting file names we want to change
-            if filename.endswith("jpg"):
-                file_extension = "jpg"
-            elif filename.endswith("png"):
-                file_extension = "png"
 
             new_image_name = "{} {}.{}".format(name_files_to, image_number, file_extension)
 
             #make sure we have the directory and the name now
             filename = "{}/{}".format(images_directory, filename)
             new_image_name = "{}/{}".format(images_directory, new_image_name)
-            print("--> current filename and location = {}".format(filename))
-            print("--> new filename and location = {}".format(new_image_name))
+
+            print("\nTrying to Change...")
+            print("     --> current filename and location = {}".format(filename))
+            print("     --> new filename and location = {}".format(new_image_name))
 
             try:
                 exists = os.path.isfile(new_image_name)
                 if exists:
-                    print("file exists not changing the name")
+                    print("     Error: File exists : NOT changing the name")
                 else:
                     os.rename(filename, new_image_name)
                     file_changed_counter += 1
             except:
-                print("file name exists already: {}".format(new_image_name))
+                print("     Error: File name exists already: {}".format(new_image_name))
 
-    print("Changed {0} files.".format(file_changed_counter))
+    print("\nChanged {0} files.\n".format(file_changed_counter))
 
 if __name__== "__main__":
     main()
